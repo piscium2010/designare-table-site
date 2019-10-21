c158ed11fdaddc1969f295c0583a9b164e178eb3
X S <xs@Xs-iMac.local>
2019-10-22 00:43:14 +0800

    sp

--- client/src/examples/Expansion/Basic.example.jsx
+++ client/src/examples/Expansion/Basic.example.jsx
@@ -1,41 +1,45 @@
 import React, { useState } from 'react'
-import Table, { Thead, Tbody, Td } from 'designare-table'
+import Table, { Thead, Tbody, Td, Icons } from 'designare-table'
 
 const data = [
-    { name: 'Johnson & Johnson', last: 135.7, chg: 2.33, chgp: 1.75, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Cisco Systems Inc.', last: 46.79, chg: 0.43, chgp: 0.93, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Walt Disney Co.', last: 130.86, chg: 1.10, chgp: 0.85, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Coca-Cola Co.', last: 53.49, chg: -0.02, chgp: -0.04, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Walmart Inc.', last: 119.42, chg: -0.11, chgp: -0.09, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.'}
+    { name: 'Johnson & Johnson', last: 135.7, chg: 2.33, chgp: 1.75, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Cisco Systems Inc.', last: 46.79, chg: 0.43, chgp: 0.93, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Walt Disney Co.', last: 130.86, chg: 1.10, chgp: 0.85, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Coca-Cola Co.', last: 53.49, chg: -0.02, chgp: -0.04, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Walmart Inc.', last: 119.42, chg: -0.11, chgp: -0.09, desc: 'Lorem ipsum, dolor sit amet.' }
 ]
+
+const style={display: 'flex', alignItems: 'center', userSelect: 'none', cursor:'pointer'}
 
 export default function () {
     const [selectedKeys, setSelectedKeys] = useState([])
     const onToggle = (evt, index) => {
         const s = new Set(selectedKeys)
-        evt.target.checked ? s.add(index) : s.delete(index)
+        s.has(index) ? s.delete(index) : s.add(index)
         setSelectedKeys([...s])
     }
-
     return (
-        <div>
         <Table
             columns={[
                 {
                     Header: '',
                     Cell: ({ rowIndex }) => {
-                        const checked = selectedKeys.includes(rowIndex)
+                        const collapsed = selectedKeys.includes(rowIndex)
                         return (
                             <Td>
-                                <input
-                                    type='checkbox'
-                                    onChange={evt => onToggle(evt, rowIndex)}
-                                    checked={checked}
-                                />
+                                <div
+                                    className='designare-transition'
+                                    style={{...style, color: collapsed ? '#1890ff':'gray'}}
+                                    onClick={evt => onToggle(evt, rowIndex)}>
+                                    {
+                                        collapsed
+                                            ? <Icons.MinusSquare style={{ width: 14 }} />
+                                            : <Icons.PlusSquare style={{ width: 14 }} />
+                                    }
+                                </div>
                             </Td>
                         )
-                    },
-                    width: 30
+                    }
                 },
                 {
                     Header: 'COMPANY',
@@ -67,52 +71,5 @@ export default function () {
                 }}
             />
         </Table>
-        
-        <Table
-            columns={[
-                {
-                    Header: '',
-                    Cell: ({ rowIndex }) => {
-                        const checked = selectedKeys.includes(rowIndex)
-                        return (
-                            <Td>
-                                <input
-                                    type='checkbox'
-                                    onChange={evt => onToggle(evt, rowIndex)}
-                                    checked={checked}
-                                />
-                            </Td>
-                        )
-                    },
-                    width: 30
-                },
-                {
-                    Header: 'COMPANY',
-                    dataKey: 'name'
-                },
-                {
-                    Header: 'LAST',
-                    dataKey: 'last'
-                },
-                {
-                    Header: 'CHG',
-                    dataKey: 'chg'
-                },
-                {
-                    Header: 'CHG %',
-                    dataKey: 'chgp'
-                }
-            ]}
-            data={data}
-        >
-            <Thead />
-            <Tbody
-                tr={({ row, rowIndex, cells, getColumns }) => {
-                    const Desc = () =>  <tr><td colSpan={getColumns().length}>{row['desc']}</td></tr>
-                    return [<tr key={0}>{cells}</tr>, <Desc key={1} />]
-                }}
-            />
-        </Table>
-        </div>
     )
 }
--- client/src/examples/Expansion/Basic.md.jsx
+++ client/src/examples/Expansion/Basic.md.jsx
@@ -2,11 +2,11 @@ const md = `import React, { useState } from 'react'
 import Table, { Thead, Tbody, Td } from 'designare-table'
 
 const data = [
-    { name: 'Johnson & Johnson', last: 135.7, chg: 2.33, chgp: 1.75, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Cisco Systems Inc.', last: 46.79, chg: 0.43, chgp: 0.93, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Walt Disney Co.', last: 130.86, chg: 1.10, chgp: 0.85, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Coca-Cola Co.', last: 53.49, chg: -0.02, chgp: -0.04, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.' },
-    { name: 'Walmart Inc.', last: 119.42, chg: -0.11, chgp: -0.09, desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique, natus.'}
+    { name: 'Johnson & Johnson', last: 135.7, chg: 2.33, chgp: 1.75, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Cisco Systems Inc.', last: 46.79, chg: 0.43, chgp: 0.93, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Walt Disney Co.', last: 130.86, chg: 1.10, chgp: 0.85, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Coca-Cola Co.', last: 53.49, chg: -0.02, chgp: -0.04, desc: 'Lorem ipsum, dolor sit amet.' },
+    { name: 'Walmart Inc.', last: 119.42, chg: -0.11, chgp: -0.09, desc: 'Lorem ipsum, dolor sit amet.' }
 ]
 
 export default function () {
@@ -18,24 +18,21 @@ export default function () {
     }
 
     return (
-        <div>
         <Table
             columns={[
                 {
                     Header: '',
                     Cell: ({ rowIndex }) => {
-                        const checked = selectedKeys.includes(rowIndex)
                         return (
                             <Td>
                                 <input
                                     type='checkbox'
                                     onChange={evt => onToggle(evt, rowIndex)}
-                                    checked={checked}
+                                    checked={selectedKeys.includes(rowIndex)}
                                 />
                             </Td>
                         )
-                    },
-                    width: 30
+                    }
                 },
                 {
                     Header: 'COMPANY',
@@ -67,53 +64,7 @@ export default function () {
                 }}
             />
         </Table>
-        </div>
-        <Table
-            columns={[
-                {
-                    Header: '',
-                    Cell: ({ rowIndex }) => {
-                        const checked = selectedKeys.includes(rowIndex)
-                        return (
-                            <Td>
-                                <input
-                                    type='checkbox'
-                                    onChange={evt => onToggle(evt, rowIndex)}
-                                    checked={checked}
-                                />
-                            </Td>
-                        )
-                    },
-                    width: 30
-                },
-                {
-                    Header: 'COMPANY',
-                    dataKey: 'name',
-                    width: '*'
-                },
-                {
-                    Header: 'LAST',
-                    dataKey: 'last'
-                },
-                {
-                    Header: 'CHG',
-                    dataKey: 'chg'
-                },
-                {
-                    Header: 'CHG %',
-                    dataKey: 'chgp'
-                }
-            ]}
-            data={data}
-        >
-            <Thead />
-            <Tbody
-                tr={({ row, rowIndex, cells, getColumns }) => {
-                    const Desc = () =>  <tr><td colSpan={getColumns().length}>{row['desc']}</td></tr>
-                    return [<tr key={0}>{cells}</tr>, <Desc key={1} />]
-                }}
-            />
-        </Table>
     )
-}`
+}
+`
 export default md
