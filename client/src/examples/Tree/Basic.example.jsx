e58ed1db5fc2531769de4e099b4eba1f5b79f18b
piscium2010 <541032442@qq.com>
2019-11-19 00:33:22 +0800

    sp

--- client/src/examples/Tree/Basic.example.jsx
+++ client/src/examples/Tree/Basic.example.jsx
@@ -99,7 +99,7 @@ export default function () {
                         dataKey: 'chg'
                     },
                     {
-                        Header: 'CHG %',
+                        Header: 'CHG%',
                         dataKey: 'chgp'
                     }
                 ]}
