3c4ad853d1de74a209a9cd9f2b51d6d7d6661c70
piscium2010 <541032442@qq.com>
2019-11-25 22:44:48 +0800

    sp

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -12,9 +12,9 @@ module.exports = {
         path: path.resolve(__dirname, 'public')
     },
     resolve: {
-        alias: {
-            'designare-table': path.join(__dirname, '../../designare-table/dist')
-        },
+        // alias: {
+        //     'designare-table': path.join(__dirname, '../../designare-table/dist')
+        // },
         modules:[path.join(__dirname, './node_modules')],
         extensions: ['.js', '.jsx']
     },
